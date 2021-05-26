971536103d1ad3166fae74d0d8d7b4baf7df33c2
durantham101 <ayush.manojk@gmail.com>
2021-05-26 20:22:45 +0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ var app = express();
 console.log('Hello World');
 
 app.get("/", (req, res) => {
-    res.send("Hello Express");
+    res.sendFile(__dirname +"/public/index.html");
 });
 
  module.exports = app;
